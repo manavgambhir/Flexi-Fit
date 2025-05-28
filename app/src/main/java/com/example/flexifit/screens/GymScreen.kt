cb20dc8b75cf2c8b37ee50bf6353b7a008b8f784
Manav Gambhir <manavgambhir11@gmail.com>
2025-05-28 17:12:44 +0530

    gym feature - added all the necessary touch interactions and overlays

--- app/src/main/java/com/example/flexifit/screens/GymScreen.kt
+++ app/src/main/java/com/example/flexifit/screens/GymScreen.kt
@@ -1,26 +1,45 @@
 package com.example.flexifit.screens
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.flexifit.R
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
@@ -52,23 +71,173 @@ fun GymScreen(navController: NavHostController){
             state = pagerState,
             modifier = Modifier.fillMaxSize()
         ) { page->
-            val imageRes = when(page){
+            var imageRes by remember { mutableIntStateOf(R.drawable.front_body) }
+            imageRes = when(page){
                 0-> R.drawable.front_body
                 1-> R.drawable.back_body
                 else -> { R.drawable.front_body }
             }
 
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ){
+            BoxWithConstraints(modifier = Modifier.fillMaxSize().padding(24.dp)) {
+                val interactionSource = remember { MutableInteractionSource() }
+                val density = LocalDensity.current
+                val imageRatio = 3f / 4f
+                val availableWidth = constraints.maxWidth.toFloat()
+                val imageHeight = availableWidth / imageRatio
+                val imageWidth = availableWidth
+
+                val verticalPadding = (constraints.maxHeight.toFloat() - imageHeight) / 2
+
                 Image(
                     painter = painterResource(id = imageRes),
                     contentDescription = "Tab Image",
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(24.dp)
+                        .align(Alignment.Center)
+                        .aspectRatio(imageRatio)
+                        .fillMaxWidth()
                 )
+
+                with(density) {
+                    @Composable
+                    fun overlayBox(x: Float, y: Float, w: Float, h: Float, color: Color, onClick: () -> Unit) {
+                        Box(
+                            modifier = Modifier
+                                .absoluteOffset(
+                                    x = (x * imageWidth).toDp(),
+                                    y = (verticalPadding + y * imageHeight).toDp()
+                                )
+                                .size(
+                                    width = (w * imageWidth).toDp(),
+                                    height = (h * imageHeight).toDp()
+                                )
+                                .background(color)
+                                .clickable(interactionSource = interactionSource, indication = null) {
+                                    onClick()
+                                }
+                        )
+                    }
+
+                    if(page==0){
+                        overlayBox(0.393f, 0.20f, 0.21f, 0.09f, Color.Transparent) {
+                            // Handle chest click
+                            imageRes = R.drawable.chest
+                        }
+                        overlayBox(0.425f, 0.29f, 0.15f, 0.2f, Color.Transparent) {
+                            // Handle abs click
+                            imageRes = R.drawable.abs
+                        }
+                        overlayBox(0.30f, 0.184f, 0.1f, 0.08f, Color.Transparent) {
+                            // Left shoulder
+                            imageRes = R.drawable.left_shoulder
+                        }
+                        overlayBox(0.6f, 0.184f, 0.1f, 0.08f, Color.Transparent) {
+                            // Right shoulder
+                            imageRes = R.drawable.right_shoulder
+                        }
+                        overlayBox(0.35f, 0.49f, 0.12f, 0.22f, Color.Transparent) {
+                            // Left leg
+                            imageRes = R.drawable.left_quad
+                        }
+                        overlayBox(0.53f, 0.49f, 0.12f, 0.22f, Color.Transparent) {
+                            // Right leg
+                            imageRes = R.drawable.right_quad
+                        }
+                        overlayBox(0.38f, 0.15f, 0.08f, 0.05f, Color.Transparent) {
+                            // Left Trap
+                            imageRes = R.drawable.traps
+                        }
+                        overlayBox(0.54f, 0.15f, 0.08f, 0.05f, Color.Transparent) {
+                            // Right Trap
+                            imageRes = R.drawable.traps
+                        }
+                        overlayBox(0.26f, 0.26f, 0.11f, 0.1f, Color.Transparent) {
+                            // Left Bicep
+                            imageRes = R.drawable.left_bicep
+                        }
+                        overlayBox(0.63f, 0.26f, 0.11f, 0.1f, Color.Transparent) {
+                            // Right Bicep
+                            imageRes = R.drawable.right_bicep
+                        }
+                        overlayBox(0.17f, 0.32f, 0.12f, 0.13f, Color.Transparent) {
+                            // Left forearm
+                            imageRes = R.drawable.forearm_left
+                        }
+                        overlayBox(0.71f, 0.32f, 0.12f, 0.13f, Color.Transparent) {
+                            // Right forearm
+                            imageRes = R.drawable.forearm_right
+                        }
+                        scope.launch {
+                            delay(200)
+                            imageRes = R.drawable.front_body
+                        }
+                    }
+                    else{
+                        overlayBox(0.30f, 0.184f, 0.13f, 0.07f, Color.Transparent) {
+                            // Left shoulder
+                            imageRes = R.drawable.left_shoulder_back
+                        }
+                        overlayBox(0.57f, 0.184f, 0.13f, 0.07f, Color.Transparent) {
+                            // Right shoulder
+                            imageRes = R.drawable.right_shoulder_back
+                        }
+                        overlayBox(0.26f, 0.25f, 0.11f, 0.1f, Color.Transparent) {
+                            // Left Tricep
+                            imageRes = R.drawable.tricep_left
+                        }
+                        overlayBox(0.63f, 0.25f, 0.11f, 0.1f, Color.Transparent) {
+                            // Right Tricep
+                            imageRes = R.drawable.tricep_right
+                        }
+                        overlayBox(0.17f, 0.33f, 0.12f, 0.13f, Color.Transparent) {
+                            // Left forearm
+                            imageRes = R.drawable.forearm_left_back
+                        }
+                        overlayBox(0.71f, 0.33f, 0.12f, 0.13f, Color.Transparent) {
+                            // Right forearm
+                            imageRes = R.drawable.forearm_right_back
+                        }
+                        overlayBox(0.44f, 0.21f, 0.12f, 0.11f, Color.Transparent) {
+                            // Mid back
+                            imageRes = R.drawable.traps_mid_back
+                        }
+                        overlayBox(0.36f, 0.25f, 0.12f, 0.19f, Color.Transparent) {
+                            // Left back
+                            imageRes = R.drawable.left_lat
+                        }
+                        overlayBox(0.52f, 0.25f, 0.12f, 0.19f, Color.Transparent) {
+                            // Right back
+                            imageRes = R.drawable.right_lat
+                        }
+                        overlayBox(0.4f, 0.43f, 0.2f, 0.11f, Color.Transparent) {
+                            // Glutes
+                            imageRes = R.drawable.hips
+                        }
+                        overlayBox(0.46f, 0.31f, 0.08f, 0.12f, Color.Transparent) {
+                            // Mid lower back
+                            imageRes = R.drawable.lower_back
+                        }
+                        overlayBox(0.35f, 0.53f, 0.12f, 0.18f, Color.Transparent) {
+                            // Left leg
+                            imageRes = R.drawable.left_glute
+                        }
+                        overlayBox(0.53f, 0.53f, 0.12f, 0.18f, Color.Transparent) {
+                            // Right leg
+                            imageRes = R.drawable.right_glute
+                        }
+                        overlayBox(0.34f, 0.74f, 0.1f, 0.11f, Color.Transparent) {
+                            // Left leg
+                            imageRes = R.drawable.left_calve_back
+                        }
+                        overlayBox(0.56f, 0.74f, 0.1f, 0.11f, Color.Transparent) {
+                            // Right leg
+                            imageRes = R.drawable.right_calve_back
+                        }
+                        scope.launch {
+                            delay(200)
+                            imageRes = R.drawable.back_body
+                        }
+                    }
+                }
             }
         }
     }
@@ -77,5 +246,5 @@ fun GymScreen(navController: NavHostController){
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GymScreenPreview(){
-//    GymScreen()
+    GymScreen(rememberNavController())
 }
